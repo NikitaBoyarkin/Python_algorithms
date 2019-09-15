b4a220b5b0f25010a030dd345e0894ff8458476b
NikitaBoyarkin <boyarkin_yandex@mail.ru>
2019-09-15 19:07:24 +0300

    first commit

---
+++ bogo sort.py
@@ -0,0 +1,22 @@
+from random import randint,shuffle
+
+def create_array(size=10,max=50):
+	return [randint(0,max) for _ in range(size)]
+
+def is_sorted(a):
+	for i in range(1,len(a)):
+		if a[i]<a[i-1]: return False
+	return True
+
+def bogo_sort(a):
+	ct=0
+	while not is_sorted(a):
+		shuffle(a)
+		ct+=1
+	return ct,a
+
+a=create_array(10,10)
+print ("Unsorted:",a)
+ct,s=bogo_sort(a)
+print ("Sorted:  ",s)
+print("Number of iteration: %d"%ct)
